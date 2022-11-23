63ae20a507162aee39e9268b8da7ab7ed75fa374
shiv4488 <shivanshpanthari5858@gmail.com>
2022-11-23 19:59:37 +0530

    1.0.0

---
+++ mainApp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register((Maincategory,Subcategory,Brand,
+                    Product,Buyer,Wishlist,Checkout,
+                    CheckoutProducts,Contact))
+
